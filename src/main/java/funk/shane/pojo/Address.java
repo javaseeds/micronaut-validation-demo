759549c6dc5d1e57257089e934ede78383562f1c
Shane Funk <shanefunk@protonmail.com>
2019-08-08 21:18:42 -0500

    Getters/Setters

--- src/main/java/funk/shane/pojo/Address.java
+++ src/main/java/funk/shane/pojo/Address.java
@@ -15,6 +15,105 @@ public class Address {
     private String postal_code;
     private String country;
 
+    public Address(String street_line1, String street_line2, String street_line3, String city, String state_full, String state_code, String postal_code, String country) {
+        this.street_line1 = street_line1;
+        this.street_line2 = street_line2;
+        this.street_line3 = street_line3;
+        this.city = city;
+        this.state_full = state_full;
+        this.state_code = state_code;
+        this.postal_code = postal_code;
+        this.country = country;
+    }
+
+    public String getStreet_line3() {
+        return this.street_line3;
+    }
+
+    public void setStreet_line3(String street_line3) {
+        this.street_line3 = street_line3;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState_full() {
+        return this.state_full;
+    }
+
+    public void setState_full(String state_full) {
+        this.state_full = state_full;
+    }
+
+    public String getState_code() {
+        return this.state_code;
+    }
+
+    public void setState_code(String state_code) {
+        this.state_code = state_code;
+    }
+
+    public String getPostal_code() {
+        return this.postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Address street_line1(String street_line1) {
+        this.street_line1 = street_line1;
+        return this;
+    }
+
+    public Address street_line2(String street_line2) {
+        this.street_line2 = street_line2;
+        return this;
+    }
+
+    public Address street_line3(String street_line3) {
+        this.street_line3 = street_line3;
+        return this;
+    }
+
+    public Address city(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public Address state_full(String state_full) {
+        this.state_full = state_full;
+        return this;
+    }
+
+    public Address state_code(String state_code) {
+        this.state_code = state_code;
+        return this;
+    }
+
+    public Address postal_code(String postal_code) {
+        this.postal_code = postal_code;
+        return this;
+    }
+
+    public Address country(String country) {
+        this.country = country;
+        return this;
+    }
+
     public Address() { /* */ }
  
     public String getStreet_line1() {
@@ -45,5 +144,4 @@ public class Address {
     public boolean equals(final Object obj) {
         return EqualsBuilder.reflectionEquals(this, obj);
     }
-
 }
