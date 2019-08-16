291a827777efba75e965f227cfa811881d0a78dd
Shane Funk <shanefunk@protonmail.com>
2019-08-15 21:56:18 -0500

    Incorporating tests

--- src/test/java/funk/shane/pojo/PhoneTest.java
+++ src/test/java/funk/shane/pojo/PhoneTest.java
@@ -1,6 +1,14 @@
 package funk.shane.pojo;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PhoneTest {
 
+    @Test
+    void testHappyPath() {
+        String x = "Dog";
+        assertThat(x).isEqualTo("Dog");
+    }
     
 }
