6c368105778d989893315fb2d5c702014f89347e
Shane Funk <shanefunk@protonmail.com>
2019-08-28 21:28:59 -0500

    wire in tests

--- src/test/java/funk/shane/pojo/PhoneTest.java
+++ src/test/java/funk/shane/pojo/PhoneTest.java
@@ -1,14 +1,22 @@
 package funk.shane.pojo;
 
-import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+
+import io.micronaut.test.annotation.MicronautTest;
+import io.micronaut.validation.validator.Validator;
+
+@MicronautTest
 public class PhoneTest {
+
+    @Inject
+    Validator validator;
 
     @Test
     void testHappyPath() {
-        String x = "Dog";
-        assertThat(x).isEqualTo("Dog");
+        
     }
-    
 }
---
+++ src/test/java/funk/shane/util/Utils.java
@@ -0,0 +1,9 @@
+package funk.shane.util;
+
+import io.micronaut.jackson.ObjectMapperFactory;
+
+public class Utils {
+    private static final ObjectMapperFactory mapper = new ObjectMapperFactory();
+    
+    
+}
