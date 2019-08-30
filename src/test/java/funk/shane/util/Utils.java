11eb93c773e8b5b7e31bc42bb3a1ef08278a0f0e
Shane Funk <shanefunk@protonmail.com>
2019-08-30 06:56:38 -0500

    figuring out how to get resources from filesystem in Micronaut

--- src/test/java/funk/shane/util/Utils.java
+++ src/test/java/funk/shane/util/Utils.java
@@ -1,9 +1,20 @@
 package funk.shane.util;
 
+import java.util.Optional;
+
+import io.micronaut.core.io.ResourceResolver;
+import io.micronaut.core.io.scan.ClassPathResourceLoader;
 import io.micronaut.jackson.ObjectMapperFactory;
 
 public class Utils {
     private static final ObjectMapperFactory mapper = new ObjectMapperFactory();
     
-    
+    public static String readResource(final String fileName) {
+        ClassPathResourceLoader loader = 
+          new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
+          Optional<String> resource = loader.getResourceAsStream("classpath:");
+        return "";
+// return FileUtils.readFileToString(new ClassPathResource("/com/springventuregroup/maestro/" + fileName, Utils.class.getClass()).getFile(),
+// 			StandardCharsets.UTF_8);    
+    }
 }
