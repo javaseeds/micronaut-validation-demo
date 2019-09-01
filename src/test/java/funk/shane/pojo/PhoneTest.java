26f9a7312c3d272ee392fdde302f9c5b59fa4cc9
Shane Funk <shanefunk@protonmail.com>
2019-08-31 22:22:49 -0500

    Adding in Resource reader

--- src/test/java/funk/shane/pojo/PhoneTest.java
+++ src/test/java/funk/shane/pojo/PhoneTest.java
@@ -1,13 +1,18 @@
 package funk.shane.pojo;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import javax.inject.Inject;
 
 import org.junit.jupiter.api.Test;
 
+import funk.shane.util.Utils;
 import io.micronaut.test.annotation.MicronautTest;
 import io.micronaut.validation.validator.Validator;
+import lombok.extern.slf4j.Slf4j;
 
 @MicronautTest
+@Slf4j
 public class PhoneTest {
 
     @Inject
@@ -15,6 +20,8 @@ public class PhoneTest {
 
     @Test
     void testHappyPath() {
-        
+        final Phone testPhone = Utils.getClassFromJsonResource(Phone.class, "phone-1.json");
+        assertNotNull(testPhone);
+        log.info("testPhone: {}", testPhone);
     }
 }
--- src/test/java/funk/shane/util/Utils.java
+++ src/test/java/funk/shane/util/Utils.java
@@ -1,20 +1,56 @@
 package funk.shane.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
+
+import javax.inject.Singleton;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import org.apache.commons.io.IOUtils;
 
 import io.micronaut.core.io.ResourceResolver;
 import io.micronaut.core.io.scan.ClassPathResourceLoader;
-import io.micronaut.jackson.ObjectMapperFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@Singleton
 public class Utils {
-    private static final ObjectMapperFactory mapper = new ObjectMapperFactory();
-    
-    public static String readResource(final String fileName) {
-        ClassPathResourceLoader loader = 
-          new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
-          Optional<String> resource = loader.getResourceAsStream("classpath:");
-        return "";
-// return FileUtils.readFileToString(new ClassPathResource("/com/springventuregroup/maestro/" + fileName, Utils.class.getClass()).getFile(),
-// 			StandardCharsets.UTF_8);    
+  private static final ObjectMapper mapper = 
+    new ObjectMapper().disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+  public static String readResource(final String fileName) {
+    log.info("fileName: {}", fileName);
+    ClassPathResourceLoader loader = new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
+    Optional<InputStream> is = loader.getResourceAsStream("classpath:funk/shane/pojo/" + fileName);
+
+    String resource = null;
+    if (is.isPresent()) {
+      try {
+        resource = IOUtils.toString(is.get(), StandardCharsets.UTF_8);
+      } catch (IOException e) {
+        log.error("bad juju: {}", e.getMessage(), e);
+      }
+    } else {
+      log.warn("The load from resource did not work");
     }
+
+    return resource;
+  }
+
+  public static <T> T getClassFromJsonResource(Class<T> klass, String jsonFile) {
+    log.info("jsonFile [{}] for Class [{}]", jsonFile, klass.getSimpleName());
+    T object = null;
+
+    try {
+      object = mapper.readValue(readResource(jsonFile), klass);
+    } catch (IOException e) {
+      log.error("bad juju reading file: [{}] {}", jsonFile, e.getMessage(), e);
+    }
+
+    return object;
+  }
 }
